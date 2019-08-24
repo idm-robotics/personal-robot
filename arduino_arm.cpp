43532d651370d1b03a155b33b4a21bf00c11a1ba
Denis Grachev <dgrachev28@gmail.com>
2019-08-24 03:00:39 +0300

    Calibrate base, shoulder1 and shoulder2

--- arduino_arm.cpp
+++ arduino_arm.cpp
@@ -9,7 +9,7 @@
 #include <std_msgs/UInt16.h>
 #include <sensor_msgs/JointState.h>
 
-ros::NodeHandle  nh;
+ros::NodeHandle nh;
 
 double radiansToDegrees(float position_radians) {
   position_radians = position_radians + 1.6;
@@ -27,9 +27,16 @@ struct Joint {
     servo.attach(pin);
   }
   
-  spin(double radia_angle) {
-    angle = radiansToDegrees(radian_angle)
-    double degree_change = (angle - calib)/mult_calib;
+  spin(double radian_angle) {
+    
+    double angle = radiansToDegrees(radian_angle);
+    double degree_change = (angle + calib) * mult_calib;
+    
+    // nh.loginfo("arduino angles: ");
+    // char result[8]; // Buffer big enough for 7-character float
+    // dtostrf(degree_change, 6, 2, result); // Leave room for too large numbers!
+    // nh.loginfo(result);
+    
     servo.write(degree_change);
   }
 };
@@ -41,47 +48,37 @@ Joint shoulder2;
 Joint shoulder1;
 Joint base;
 
-void servo_cb(const sensor_msgs::JointState& cmd_msg){
+void servo_cb(const sensor_msgs::JointState& cmd_msg) {
   char result[8]; // Buffer big enough for 7-character float
-  for (byte i = 0; i < 2; ++i) {
+  for (byte i = 0; i < 3; ++i) {
     dtostrf(radiansToDegrees(cmd_msg.position[i]), 6, 2, result); // Leave room for too large numbers!
     nh.loginfo(result);
   }
-  base.spin(cmd_msg.position[0])
-  shoulder1.spin(cmd_msg.position[1])
-  // TODO: calibrate elbow, wrist, grippper
-  nh.loginfo("WIN");  
+  base.spin(cmd_msg.position[0]);
+  shoulder1.spin(cmd_msg.position[1]);
+  shoulder2.spin(cmd_msg.position[2]);
+  elbow.spin(cmd_msg.position[3]);
+
+  nh.loginfo("finish step");
 }
 
-ros::Subscriber<sensor_msgs::JointState> sub("joint_states", servo_cb);
+ros::Subscriber<sensor_msgs::JointState> sub("/joint_states", servo_cb);
 
-void setup(){
+void setup() {
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(sub);
 
-  base = Joint(2, -90, 8/9);
-  shoulder1 = Joint(4, 180, 8/9);
-  // shoulder2 = Joint(6, 50, 8/9);
-  // gripper = Joint(8, x, 8/9);
+  base = Joint(2, -70, 8.0/9);
+  shoulder1 = Joint(4, -90, 8.0/9);
+  shoulder2 = Joint(6, 10, 1);
+  elbow = Joint(7, 0, 1); // TODO: calibrate elbow (elbow 25kg servo needs external supply)
   // wrist = Joint(11, x, 8/9);
-  // elbow = Joint(6, x, 8/9);
-
+  // gripper = Joint(8, x, 8/9);
+  
   delay(1);
 }
 
-void loop(){
+void loop() {
   nh.spinOnce();
-  for (int i = 0; i <= 160; i+=1) {
-    base.spin(i);
-    shoulder1.spin(i);
-    delay(20);
-  }
-  delay(1000);
-  for (int i = 160; i >= 0; i-=1) {
-    base.spin(i);
-    shoulder1.spin(i);
-    delay(20);
-  }
-  delay(2000);
 }
