b9f9465ecb39a52d26965ed4a10496a20a6f4228
Denis Grachev <dgrachev28@gmail.com>
2019-11-03 17:15:54 +0300

    Transform manipulator frame of reference to kinect when grasping

--- manipulator/scripts/grasp_evaluator.py
+++ manipulator/scripts/grasp_evaluator.py
@@ -4,43 +4,48 @@ import rospy
 import moveit_commander
 from grasp_detection.msg import Grasp
 from moveit_msgs.msg import moveit_msgs, geometry_msgs
-from math import pi
+import tf
 import ik_solver_test
-
-
-# def setup_joint_config():
-#     joint_goal = group.get_current_joint_values()
-#     joint_goal[0] = 0
-#     joint_goal[1] = -pi / 4
-#     joint_goal[2] = 0
-#     joint_goal[3] = -pi / 2
-#     joint_goal[4] = 0
-#     joint_goal[5] = pi / 3
-
-#     group.go(joint_goal, wait=True)
-#     group.stop()
-
-
-# setup_joint_config()
-
-
-# robot = moveit_commander.RobotCommander()
-# scene = moveit_commander.PlanningSceneInterface()
-# group_name = "hand"
-# group = moveit_commander.MoveGroupCommander(group_name)
+from geometry_msgs.msg import PointStamped
 
 ik_solver = ik_solver_test.IKSolverTester()
+tf_listener = tf.TransformListener()
 
 # ('Goal target coordinates ', 0.027420001853080023, 0.7070000469684601, 0.010100000670978)
 
+
 def callback(grasp):
+    try:
+        (trans, rot) = tf_listener.lookupTransform('/base_link', '/camera_link', rospy.Time(0))
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        pass
+
+    print('Transform: ', trans)
+    print('Rotation: ', rot)
 
     left_point = grasp.left_point
     right_point = grasp.right_point
     x = (left_point.x + right_point.x) / 2
     y = (left_point.y + right_point.y) / 2
     z = (left_point.z + right_point.z) / 2
-    ik_solver.move_xyz(x, y, z)
+
+    point = PointStamped()
+    point.point.x = x
+    point.point.y = y
+    point.point.z = z
+    point.header.frame_id = 'camera_rgb_optical_frame'
+    print(grasp.header)
+
+    transformed_point = tf_listener.transformPoint('base_link', point)
+    print("===========Point===============")
+    print(point)
+    print("===========TRANSFORMER===============")
+    print(transformed_point)
+    ik_solver.move_xyz(transformed_point.point.x, transformed_point.point.y, transformed_point.point.z)
+
+    # x: -0.21489746767595763
+    # y: 0.3179554308020345
+    # z: -0.008753333914847445
 
     print('Goal target coordinates ', x, y, z)
     ik_solver.print_current_pose()
