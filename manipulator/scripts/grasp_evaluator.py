c0535cd2a27ada1bf2563ba7160e3b435f55a681
Denis Grachev <dgrachev28@gmail.com>
2019-11-10 19:17:25 +0300

    Refactor IKSolverTest

--- manipulator/scripts/grasp_evaluator.py
+++ manipulator/scripts/grasp_evaluator.py
@@ -1,17 +1,59 @@
 #!/usr/bin/env python2
+import moveit_commander
 import numpy as np
 import rospy
-from grasp_detection.msg import Grasp as GraspDetection
 import tf
-import ik_solver_test
-from geometry_msgs.msg import PointStamped, PoseStamped
-from moveit_msgs.msg import Grasp, GripperTranslation, PlaceLocation
+from geometry_msgs.msg import PoseStamped
+from grasp_detection.msg import Grasp as GraspDetection
+from moveit_msgs.msg import moveit_msgs
 from tf.transformations import quaternion_from_euler
+
+
+class IKSolver:
+    def __init__(self):
+        moveit_commander.roscpp_initialize(args=[])
+        rospy.init_node('ik_solver', anonymous=True)
+
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+
+        self.group_name = "arm"
+        self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
+        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                            moveit_msgs.msg.DisplayTrajectory,
+                                                            queue_size=20)
+        self.print_base_info()
+
+    def print_base_info(self):
+        planning_frame = self.move_group.get_planning_frame()
+        print("============ Planning frame: %s" % planning_frame)
+
+        eef_link = self.move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+
+        group_names = self.robot.get_group_names()
+        print("============ Available Planning Groups: %s" % group_names)
+
+    def print_current_pose(self):
+        print('====== Current Pose ======')
+        print(self.move_group.get_current_pose())
+
+    def move_xyz(self, x=0.0, y=0.0, z=0.0):
+        self.move_group.set_position_target([x, y, z])
+        self.move_group.go(wait=True)
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+
+    def move_pose(self, pose):
+        self.move_group.set_pose_target(pose)
+        self.move_group.go(wait=True)
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
 
 
 class GraspEvaluator:
     def __init__(self):
-        self.ik_solver = ik_solver_test.IKSolverTester()
+        self.ik_solver = IKSolver()
         self.tf_listener = tf.TransformListener()
 
     @staticmethod
