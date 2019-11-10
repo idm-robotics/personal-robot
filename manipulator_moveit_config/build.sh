39499b2d2b818d3305be8bba54efe8af7216d2c0
ExMiracle <exmiracle2@gmail.com>
2019-11-10 04:17:49 +0300

    Modified all build.sh scripts and corrected setup_assistant files

--- manipulator_moveit_config/build.sh
+++ manipulator_moveit_config/build.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
-CATKIN_ROOT=$HOME/ws_moveit # Replace $HOME/ws_moveit/src to your catkin workspace directory!
-CATKIN_WORKSPACE=${CATKIN_ROOT}/src 
+IFS=':' # space is set as delimiter
+read -ra ADDR <<< "$CMAKE_PREFIX_PATH" # str is read into an array as tokens separated by IFS
+for i in "${ADDR[@]}"; do # access each element of array
+    if [[ "$i" =~ devel? ]]
+    then
+      echo "CATKIN WS FOLDER WAS FOUND"
+      CATKIN="$i"
+    fi
+done
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+source $CATKIN/setup.bash
+
+CATKIN_ROOT="$(cd $(echo $CATKIN) && cd .. && pwd)"
+CATKIN_WORKSPACE=${CATKIN_ROOT}/src
 
 DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$(dirname ${DIR})
